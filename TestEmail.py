a87b26f9e9dda2f0368b9ba070a2fe80f06154cf
Sidharth Menon <sidharth_mnn@yahoo.co.in>
2018-10-30 12:00:13 +0530

    Add files via upload

---
+++ TestEmail.py
@@ -0,0 +1,26 @@
+import os
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+
+
+def SendMail(ImgFileName):
+    img_data = open(ImgFileName, 'rb').read()
+    msg = MIMEMultipart()
+    msg['Subject'] = 'subject'
+    msg['From'] = 'e@mail.cc'
+    msg['To'] = 'e@mail.cc'
+
+    text = MIMEText("test")
+    msg.attach(text)
+    image = MIMEImage(img_data, name=os.path.basename(ImgFileName))
+    msg.attach(image)
+
+    s = smtplib.SMTP(Server, Port)
+    s.ehlo()
+    s.starttls()
+    s.ehlo()
+    s.login(UserName, UserPassword)
+    s.sendmail(From, To, msg.as_string())
+    s.quit()
